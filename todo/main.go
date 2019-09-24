4730e81e0c19ec318a0431a8554a49298a5ba79d
qushiming <2969266@qq.com>
2019-09-24 16:12:40 +0800

    update

--- todo/main.go
+++ todo/main.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	beego.Router("/", &controllers.MainController{})
 	beego.Router("/task/", &controllers.TaskController{}, "get:ListTasks;post:NewTask")
+	beego.Router("/test-dca", &controllers.TaskController{}, "get:TestDca")
 	beego.Router("/task/:id:int", &controllers.TaskController{}, "get:GetTask;put:UpdateTask")
 	beego.Run()
 }
